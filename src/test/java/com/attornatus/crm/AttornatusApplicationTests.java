a9279fd589fdf30dedda2b8f7026a6a10f9b7cf7
Maria Carolina Xavier <mcs_nasc@hotmail.com>
2023-01-04 14:36:34 -0300

    Attornatus

--- src/test/java/com/attornatus/crm/AttornatusApplicationTests.java
+++ src/test/java/com/attornatus/crm/AttornatusApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AttornatusApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		AttornatusApplication.main(new String[] {});
 	}
 
 }
